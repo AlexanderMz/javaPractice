761fc44ceced87400386ccb7e269c47d4419a4e0
Alexander Mz Tilbe <AlexanderMz@users.noreply.github.com>
2021-05-08 14:09:31 -0500

    Ultimando detalles

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class App {
@@ -12,67 +14,75 @@ public class App {
         Entrada();
     }
 
-    private static void Entrada() {
-        int x = 0, y = 0, l = 1;
+    private static void Entrada() throws InterruptedException, IOException {
+        int x = 0, y = 0, l = 1, vacias = 0;
+        String result = "";
         while (true) {
             for (l = 1; l < letras.length; l++) {
                 String key = inp.nextLine();
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
                 switch (key) {
-                    case "A":
+                    case "A", "a":
                         y--;
                         if (y < 0) {
                             y = 0;
                             break;
                         }
                         letrasString[x][y] = letras[l];
-                        printTabla();
+                        // printTabla();
                         break;
-                    case "W":
+                    case "W", "w":
                         x--;
                         if (x < 0) {
                             x = 0;
                             break;
                         }
                         letrasString[x][y] = letras[l];
-                        printTabla();
+                        // printTabla();
                         break;
-                    case "S":
+                    case "S", "s":
                         x++;
                         if (x > 5) {
                             x = 5;
                             break;
                         }
                         letrasString[x][y] = letras[l];
-                        printTabla();
+                        // printTabla();
                         break;
-                    case "D":
+                    case "D", "d":
                         y++;
                         if (y > 5) {
                             y = 5;
                             break;
                         }
                         letrasString[x][y] = letras[l];
-                        printTabla();
                         break;
 
                     default:
                         return;
                 }
+                printTabla();
 
                 if (l == letras.length - 1) {
                     break;
                 }
+
             }
+            for (int f = 0; f < 6; f++)
+                for (int c = 0; c < 6; c++)
+                    vacias += Objects.isNull(letrasString[f][c]) ? 1 : 0;
+
             for (int i = 0; i < 6; i++) {
-                System.out.print(letrasString[i][i]);
+                result += Objects.isNull(letrasString[i][i]) ? "-" : letrasString[i][i];
             }
+            System.out.print(result + vacias);
         }
     }
 
     private static void printTabla() {
         for (int x = 0; x < 6; x++) {
             for (int y = 0; y < 6; y++) {
-                System.out.print(letrasString[x][y]);
+                System.out.print(Objects.isNull(letrasString[x][y]) ? "-" : letrasString[x][y]);
                 if (y != letrasString[x].length - 1)
                     System.out.print("\t");
             }
